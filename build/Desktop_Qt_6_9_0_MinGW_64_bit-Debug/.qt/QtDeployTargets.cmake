4eef0035604f38b4174a186ad438c9f18920845c
BierSimon <simonbierschneider@gmail.com>
2025-04-16 09:17:25 +0200

    Initialer Commit mit Grundstruktur

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appSonne_Test_FILE D:/Qt files/Sonne Test/Sonne_Test/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/appSonne_Test.exe)
+set(__QT_DEPLOY_TARGET_appSonne_Test_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appSonne_Test_RUNTIME_DLLS D:/Qt/6.9.0/mingw_64/bin/Qt6Quick.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6QmlMeta.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6QmlWorkerScript.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6QmlModels.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Qml.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Network.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appSonne_Test_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appSonne_Test")
+set(working_dir "D:/Qt files/Sonne Test/Sonne_Test")
+set(src_and_dest_list
+    "D:/Qt files/Sonne Test/Sonne_Test/Main.qml"
+    "D:/Qt files/Sonne Test/Sonne_Test/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Sonne_Test/Main.qml"
+
+)
+set(timestamp_file "D:/Qt files/Sonne Test/Sonne_Test/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appSonne_Test_qml.txt")
